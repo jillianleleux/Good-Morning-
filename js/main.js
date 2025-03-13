4fd81a461504e8a5a69856c584bdde66ac7d16ae
jillianleleux <jleleu9@lsu.edu>
2025-03-13 11:26:27 -0500

    video / rotate

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 // JS scripts placed here
+const video = document.querySelector(".hover-video");
+
+video.addEventListener("mouseover", () => {
+  video.play();
+});
+
+video.addEventListener("mouseout", () => {
+  video.pause();
+  video.currentTime = 0; // Reset to the beginning when hover stops
+});
